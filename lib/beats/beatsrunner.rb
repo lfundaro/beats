3aee5e9f55fb72d9d440bafb4e99a95d27b4a070
Lorenzo Fundaro <lorenzofundaro@gmail.com>
2014-05-22 00:03:21 +0200

    Implemented share_song method to upload generated wav files to a SoundCloud account

--- lib/beats/beatsrunner.rb
+++ lib/beats/beatsrunner.rb
@@ -29,10 +29,58 @@ module Beats
         AudioEngine.new(song, kit).write_to_file(output_file_name)
       end
 
-      {:duration => durations.last}
+      timeElapsed = durations.last
+
+      # Upload this song to a SoundCloud account
+      if @options[:share]
+        share_song(@output_file_name)
+      end
+
+      {:duration => timeElapsed}
     end
 
   private
+
+    # If the --share option is used, the song is uploaded in a SoundCloud account,
+    # this method will ask for account credentials.
+    def share_song(output_file_name) 
+      require 'soundcloud'
+      require 'read-password'
+      require 'spinning_cursor'
+      puts 'Please provide your SoundCloud credentials: '
+      print 'username: '
+      username = STDIN.gets
+      print 'password: '
+      password = Kernel.password()
+      begin
+        client = SoundCloud.new({
+        :client_id     => '38fcbdcfc87e6da26b58ef1be825b64b',
+        :client_secret => 'd5d50f0ce5e1b09eac18c73f8eab8cf0',
+        :username      =>  username,
+        :password      =>  password
+        })
+      rescue Exception => e
+        puts 'Could not authenticate: ' + e.message
+      end
+
+      #start uploading
+      begin
+        SpinningCursor.run do
+          banner 'Uploading file'
+          type :spinner
+          action do
+            unless client.nil? 
+              client.post('/tracks', :track => {
+                :title      => output_file_name,
+                :asset_data => File.new(output_file_name)
+              })
+            end
+          end
+        end
+        rescue Exception => e
+          puts e.message
+      end
+    end
 
     # If the -p option is used, transform the song into one whose flow consists of
     # playing that single pattern once.
